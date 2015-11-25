7ed1f680b638fef587c52a3587f591289dc79c29
Ankit <amurark@ncsu.edu>
2015-11-25 18:06:23 -0500

    changes for git and nginx

--- chef/site-cookbooks/expertiza/recipes/ssh.rb
+++ chef/site-cookbooks/expertiza/recipes/ssh.rb
@@ -15,31 +15,31 @@ end
 #end
 
 # disable root login
-bash "disable root login" do
-  code <<-EOH
-    sed -i '/PermitRootLogin.*/d' /etc/ssh/sshd_config
-    echo 'PermitRootLogin no' >> /etc/ssh/sshd_config
-  EOH
-  notifies :restart, "service[ssh]", :delayed
-  not_if "grep -xq 'PermitRootLogin no' /etc/ssh/sshd_config"
-end
+# bash "disable root login" do
+#   code <<-EOH
+#     sed -i '/PermitRootLogin.*/d' /etc/ssh/sshd_config
+#     echo 'PermitRootLogin no' >> /etc/ssh/sshd_config
+#   EOH
+#   notifies :restart, "service[ssh]", :delayed
+#   not_if "grep -xq 'PermitRootLogin no' /etc/ssh/sshd_config"
+# end
 
 # restrict login only to created user
-bash "restrict login only to created user" do
-  code <<-EOH
-    sed -i '/AllowUsers.*/d' /etc/ssh/sshd_config
-    echo 'AllowUsers #{node['user']['name']}' >> /etc/ssh/sshd_config
-  EOH
-  notifies :restart, "service[ssh]", :delayed
-  not_if "grep -xq 'AllowUsers #{node['user']['name']}' /etc/ssh/sshd_config"
-end
+# bash "restrict login only to created user" do
+#   code <<-EOH
+#     sed -i '/AllowUsers.*/d' /etc/ssh/sshd_config
+#     echo 'AllowUsers #{node['user']['name']}' >> /etc/ssh/sshd_config
+#   EOH
+#   notifies :restart, "service[ssh]", :delayed
+#   not_if "grep -xq 'AllowUsers #{node['user']['name']}' /etc/ssh/sshd_config"
+# end
 
 # disable dns
-bash "disable dns" do
-  code <<-EOH
-    sed -i '/UseDNS.*/d' /etc/ssh/sshd_config
-    echo 'UseDNS no' >> /etc/ssh/sshd_config
-  EOH
-  notifies :restart, "service[ssh]", :delayed
-  not_if "grep -xq 'UseDNS no' /etc/ssh/sshd_config"
-end
+# bash "disable dns" do
+#   code <<-EOH
+#     sed -i '/UseDNS.*/d' /etc/ssh/sshd_config
+#     echo 'UseDNS no' >> /etc/ssh/sshd_config
+#   EOH
+#   notifies :restart, "service[ssh]", :delayed
+#   not_if "grep -xq 'UseDNS no' /etc/ssh/sshd_config"
+# end
