e9b248fdc983113cfab6c62a4d733ee5a8480d8e
Ankit <agarg9@ncsu.edu>
2015-11-28 20:58:10 -0500

    added package for java nad bundler, added java home path

--- chef/site-cookbooks/expertiza/attributes/default.rb
+++ chef/site-cookbooks/expertiza/attributes/default.rb
@@ -3,5 +3,9 @@ default['app']               = 'expertiza'
 default['nodejs']['dir']     = '/usr/local'
 default['nodejs']['version'] = '0.10.29'
 
-default['ruby']['version']   = '2.1.2'
-default['redis']['version']  = '2.8.13'
+default['ruby']['version']   = '2.1.5'
+#default['redis']['version']  = '2.8.13'
+
+#default['java']['java_home'] = "/usr/java/latest"
+default['java']['java_home'] = "/usr/lib/jvm/java-6-openjdk-amd64"
+default['java']['set_etc_environment'] = true
