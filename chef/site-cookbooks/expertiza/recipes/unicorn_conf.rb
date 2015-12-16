e8461fcfd0c1adb5fbece1bfeef09fef88bc47f7
amurark <amurark@ncsu.edu>
2015-12-16 12:54:04 -0500

    Update unicorn_conf.rb

--- chef/site-cookbooks/expertiza/recipes/unicorn_conf.rb
+++ chef/site-cookbooks/expertiza/recipes/unicorn_conf.rb
@@ -2,17 +2,16 @@
 #Installing the unicorn gem
 puts "==== Installing the unicorn gem!  ===="
 execute 'Install unicorn Gem' do
-  cwd '#{node[config][exp_path]}'
+  cwd "#{node['config']['exp_path']}"
   command 'gem install unicorn'
 end
 puts "==== Unicorn gem installed successfully! ===="
 
 # Setting the custom configuration for unicorn
 puts "==== Setting the custom configuration for unicorn! ===="
-template "#{node[config][exp_path]}/config/unicorn.rb" do
+template "#{node['config']['exp_path']}/config/unicorn.rb" do
   source "unicorn.rb.erb"
   mode '0755'
   owner 'root'
 end
 puts "==== Custom configuration for unicorn set successfully! ===="
-
